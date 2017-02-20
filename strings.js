aa2f9ad51b2f13f46907ecf7022804e80b465981
jmarcusgill <jmarcusgill@gmail.com>
2017-02-20 13:41:31 -0600

    added function to check if user is using numbers or letters

--- strings.js
+++ strings.js
@@ -1,28 +1,50 @@
+//Grabbing and Declaring Variable from HTML
 var userInput = document.getElementById("user-input");
-var submitButton = document.getElementById("submit-button")
-var clearButton = document.getElementById("clear-button")
-var outputDiv = document.getElementById("output")
+var submitButton = document.getElementById("submit-button");
+var clearButton = document.getElementById("clear-button");
+var outputDiv = document.getElementById("output");
+
+//Setting placeholder variable to be used in outputResults() function
 var testString = "";
 
-submitButton.addEventListener("click", outputResults)
-clearButton.addEventListener("click", clearOutput)
+//Adding Event Listeners to Buttons and Enter Key
+submitButton.addEventListener("click", checkIfNumber);
+clearButton.addEventListener("click", clearOutput);
+window.addEventListener("keypress", checkEnter);
 
+//Function for the keypress, references the output function
+function checkEnter (e) {
+  if (e.keyCode === 13) {
+    checkIfNumber();
+  }
+};
 
+//Checks to see if userInput is only letters, not numbers
+function checkIfNumber() {
+  if (isNaN(userInput.value)) {
+    outputResults();
+  } else {
+    alert("Enter only letters please")
+  }
+}
 
+//Clears the DOM and input field
 function clearOutput(string) {
   outputDiv.innerHTML = "";
   userInput.value = "";
-}
+};
 
+//Reverses the userInput
 function reversal(string) {
   return string.split("").reverse().join("");
+};
 
-}
-
+//Sorts the userInout
 function alphabits(string) {
   return string.split("").sort().join("");
-}
+};
 
+//Checks to see if userInput is a palindrome
 function palindrome(string) {
   var stringReversed = string.split("").reverse().join("");
   if (stringReversed === string) {
@@ -30,12 +52,12 @@ function palindrome(string) {
   } else {
     return "Your word is NOT a palindrome"
   }
-}
+};
 
+//Outputs all three functions to DOM and sets testString to userInput value
 function outputResults() {
   testString = userInput.value;
   outputDiv.innerHTML += "<div>" + "Your word reversed: " + reversal(testString) + "</div>";
   outputDiv.innerHTML += "<div>" + "Your word sorted: " + alphabits(testString) + "</div>";
   outputDiv.innerHTML += "<div>" + palindrome(testString) + "</div>";
 };
-
